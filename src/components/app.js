618a749bd5cbea3bcdc01e9dad89dca77c99fc0b
joseph <codemasterhtml@gmail.com>
2018-08-08 17:40:13 +0000

    added text

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="grid">
-       
+       <h1 className="grid__title">Birthday Countdown</h1>
        
        <div className="grid__skew-dark"></div>
        <div className="grid__skew-light"></div>
